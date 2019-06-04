9313ccd1419487c09bff102ce034f9e3e8bd9d28
kaikouxiao <kaikouxiao@2dfire.com>
2019-06-04 15:27:56 +0800

    解决基本模板

--- wechat/g.js
+++ wechat/g.js
@@ -1,7 +1,7 @@
 var sha1 = require("sha1");
 var Accessid = require("./require")
 var getRowBody = require("raw-body")
-var xml2js =require("./xml2js")
+var xml2js = require("./xml2js")
 module.exports = function (opts) {
     var access = new Accessid(opts)
     return function* (next) {
@@ -12,14 +12,13 @@ module.exports = function (opts) {
         var nonce = this.query.nonce
         var str = [token, timestamp, nonce].sort().join('')
         var sha = sha1(str)
-        console.log('this', this)
-        console.log(this.method)
-        console.log(this.request.method)
+        var _this = this
         if (this.request.method === 'GET') {
             if (sha === signature) {
+                console.log('验证成功')
                 this.body = echostr + ''
             } else (
-                this.body = 'woron'
+                _this.response.body = 'woron 002315165sadasdsda0202031dAFSADCVSERVARADVERVADCSVAW'
             )
         } else if (this.request.method === 'POST') {
             if (sha !== signature) {
@@ -31,8 +30,73 @@ module.exports = function (opts) {
                 limit: '10mb',
                 encoding: 'utf-8'
             })
-            var centent = yield xml2js(data)
-            console.log('data', centent)
+            var centent = yield xml2js.gatUserData(data)
+
+            var data = yield xml2js.formatCentent(centent.xml)
+            console.log(data)
+            var text = '欢迎关注我的公众号哦！！'
+            if (data.MsgType === 'event') {
+                if (data.Event === 'subscribe') {
+                    var now = new Date().getTime()
+                    if (data.FromUserName === 'oO9M05iHPC6bEqQww9OzGxqvHb7c') {
+                        _this.response.status = 200
+                        _this.response.type = 'application/xml'
+                        _this.response.body = ('<xml>' +
+                            '<ToUserName><![CDATA[' + data.FromUserName + ']]></ToUserName>' +
+                            '<FromUserName><![CDATA[' + data.ToUserName + ']]></FromUserName>' +
+                            '<CreateTime>' + now + '</CreateTime>' +
+                            '<MsgType><![CDATA[text]]></MsgType>' +
+                            '<Content><![CDATA[霞姐，欢迎关注我的公众号哦！]]></Content>' +
+                            '</xml>')
+                        return
+                    }
+                    if (data.FromUserName === 'oO9M05lUR5nGNxRIChnijtGNms94') {
+                        _this.response.status = 200
+                        _this.response.type = 'application/xml'
+                        _this.response.body = ('<xml>' +
+                            '<ToUserName><![CDATA[' + data.FromUserName + ']]></ToUserName>' +
+                            '<FromUserName><![CDATA[' + data.ToUserName + ']]></FromUserName>' +
+                            '<CreateTime>' + now + '</CreateTime>' +
+                            '<MsgType><![CDATA[text]]></MsgType>' +
+                            '<Content><![CDATA[嗨婷婷，欢迎关注我的公众号哦！]]></Content>' +
+                            '</xml>')
+                    }
+                    _this.response.status = 200
+                    _this.response.type = 'application/xml'
+                    _this.response.body = ('<xml>' +
+                        '<ToUserName><![CDATA[' + data.FromUserName + ']]></ToUserName>' +
+                        '<FromUserName><![CDATA[' + data.ToUserName + ']]></FromUserName>' +
+                        '<CreateTime>' + now + '</CreateTime>' +
+                        '<MsgType><![CDATA[text]]></MsgType>' +
+                        '<Content><![CDATA[' + text + ']]></Content>' +
+                        '</xml>')
+
+                }
+            }
+            if (data.MsgType === 'text') {
+                _this.response.status = 200
+                _this.response.type = 'application/xml'
+                _this.response.body = "<xml>" +
+                    '<ToUserName><![CDATA[' + data.FromUserName + ']]></ToUserName>' +
+                    '<FromUserName><![CDATA[' + data.ToUserName + ']]></FromUserName>' +
+                    "<CreateTime>" + now + "</CreateTime>" +
+                    "<MsgType><![CDATA[text]]></MsgType>" +
+                    "<Content><![CDATA[还没开发好哈，不用试了！！！]]></Content>" +
+                    "<MsgId>" + data.MsgId + "</MsgId>" +
+                    "</xml>"
+                if (data.Content === '我爱你') {
+                    _this.response.status = 200
+                    _this.response.type = 'application/xml'
+                    _this.response.body = "<xml>" +
+                        '<ToUserName><![CDATA[' + data.FromUserName + ']]></ToUserName>' +
+                        '<FromUserName><![CDATA[' + data.ToUserName + ']]></FromUserName>' +
+                        "<CreateTime>" + now + "</CreateTime>" +
+                        "<MsgType><![CDATA[text]]></MsgType>" +
+                        "<Content><![CDATA[嘿嘿，你好骚！！！]]></Content>" +
+                        "<MsgId>" + data.MsgId + "</MsgId>" +
+                        "</xml>"
+                }
+            }
         }
     }
 }
--- wechat/xml2js.js
+++ wechat/xml2js.js
@@ -1,9 +1,45 @@
 var xml2js = require("xml2js").parseString
-module.exports = function (data) {
+exports.gatUserData = function (data) {
     return new Promise(function (resolve, reject) {
         xml2js(data, { trim: true }, function (err, result) {
-            if (err) reject(err)
-            else resolve(result)
+            if (err) {
+                reject(err)
+            } else {
+                resolve(result)
+            }
         })
+    })
+}
+function formatCentent(data) {
+    var fromartXml = {}
+    if (typeof data === 'object') {
+        var keys = Object.keys(data)
+        for (var i = 0; i < keys.length; i++) {
+            var item = data[keys[i]]
+            var keyName = keys[i]
+            if (!(item instanceof Array) || item.length === 0) {
+                continue
+            }
+            if (item.length === 1) {
+                var val = item[0]
+                if (typeof val === 'object') {
+                    fromartXml[keyName] = formatCentent(val)
+                } else {
+                    fromartXml[keyName] = (val || '').trim()
+                }
+            } else {
+                fromartXml[keyName] = []
+                for (var t = 0; t < item.length; t++) {
+                    fromartXml[keyName].push(formatCentent(item.t))
+                }
+            }
+            // console.log('aaa', keys[i], data[keys[i]])
+        }
+    }
+    return fromartXml
+}
+exports.formatCentent = function (data) {
+    return new Promise(function (resolve, reject) {
+        resolve(formatCentent(data))
     })
 }
